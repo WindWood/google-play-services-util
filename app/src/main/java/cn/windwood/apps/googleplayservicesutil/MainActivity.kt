5d41ae1d1b34c711e7d723dfe24bb9c1664ed975
windwood <windwood@gmail.com>
2019-01-24 23:55:17 +0800

    添加 Layout以及基本代码

--- app/src/main/java/cn/windwood/apps/googleplayservicesutil/MainActivity.kt
+++ app/src/main/java/cn/windwood/apps/googleplayservicesutil/MainActivity.kt
@@ -2,11 +2,43 @@ package cn.windwood.apps.googleplayservicesutil
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.android.gms.common.GoogleApiAvailability
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private val LOG_TAG = "GooglePlayServicesUtil"
+
+    private val GOOGLE_PLAY_SERVICES_PACKAGE = GoogleApiAvailability.GOOGLE_PLAY_SERVICES_PACKAGE
+    private val GOOGLE_PLAY_STORE_PACKAGE = GoogleApiAvailability.GOOGLE_PLAY_STORE_PACKAGE
+    private val GOOGLE_PLAY_SERVICES_VERSION_CODE = GoogleApiAvailability.GOOGLE_PLAY_SERVICES_VERSION_CODE
+    private val GOOGLE_SERVICES_FRAMEWORK_PACKAGE = "com.google.android.gsf"
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+
+    fun servicesInfo(view: View) {
+        val constantValue =
+            ("Services Package: $GOOGLE_PLAY_SERVICES_PACKAGE\nVersion code: $GOOGLE_PLAY_SERVICES_VERSION_CODE")
+
+        tvOutput!!.text = constantValue
+    }
+
+    fun storeInfo(view: View) {
+        val constantValue = ("Store Package: $GOOGLE_PLAY_STORE_PACKAGE")
+
+        tvOutput!!.text = constantValue
+    }
+
+    fun aboutInfo(view: View) {
+        val constantValue = ("")
+
+        tvOutput!!.text = constantValue
     }
 }
